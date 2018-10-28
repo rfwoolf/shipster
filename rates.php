6d7fef28f7996b159fe66b563f20d3942a4c7fad
biltong2go <44553808+biltong2go@users.noreply.github.com>
2018-10-29 06:59:41 +1100

    Update rates.php

--- rates.php
+++ rates.php
@@ -19,9 +19,9 @@ foreach($rates['rate']['items'] as $item) {
 // use number_format because shopify api expects the price to be "25.00" instead of just "25"
 
 // overnight shipping is 5.50 per item
-$overnight_cost = number_format($quantity * 5.50, 2, '', '');
+$overnight_cost = number_format(9.99,2,'','');//number_format($quantity * 5.50, 2, '', '');
 // regular shipping is 2.75 per item
-$regular_cost = number_format($quantity * 2.75, 2, '', '');
+$regular_cost = number_format(0,2,'','');//number_format($quantity * 2.75, 2, '', '');
 
 // overnight shipping is 1 to 2 days after today
 $on_min_date = date('Y-m-d H:i:s O', strtotime('+1 day'));
@@ -34,20 +34,12 @@ $reg_max_date = date('Y-m-d H:i:s O', strtotime('+7 days'));
 // build the array of line items using the prior values
 $output = array('rates' => array(
     array(
-        'service_name' => 'Endertech Overnight',
-        'service_code' => 'ETON',
+        'service_name' => 'Shipster',
+        'service_code' => 'Shipster',
         'total_price' => $overnight_cost,
-        'currency' => 'USD',
+        'currency' => 'AUD',
         'min_delivery_date' => $on_min_date,
         'max_delivery_date' => $on_max_date
-    ),
-    array(
-        'service_name' => 'Endertech Regular',
-        'service_code' => 'ETREG',
-        'total_price' => $regular_cost,
-        'currency' => 'USD',
-        'min_delivery_date' => $reg_min_date,
-        'max_delivery_date' => $reg_max_date
     )
 ));
 
